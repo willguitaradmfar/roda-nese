50d85e358059c9a91ca318c4a595f140ffc665b4
William Lima <willguitaradmfar@gmail.com>
2014-09-15 15:53:31 -0300

    $1

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
+logQueue.urlPost = "http://localhost:3000/";
+logQueue.objHeader.tenant = 'DESENHADOR';      
+logQueue.showMessageConsole = true;
+logQueue.qtdeMessageSent = 500;
+logQueue.debug = false;
+logQueue.trace = true;
+logQueue.sleepThread = 1000;
+logQueue.start();
+
+
+//.replace(/^(.*)(\$largura\$)(.*)$/, '$112$3')
+
 $(function () {
+
+	var palleta = $('#palleta');
+
+	for(var i in templates){
+		var template = templates[i];
+		console.debug('ADD COMPONENT TO PALLETA ('+i+')');
+		palleta.append(template.templ);
+	}
 
 	$( "#project" ).sortable({
       revert: true
     });
 
-	$('a').draggable({
+	$('.component').draggable({
 	    connectToSortable: "#project",
+	    cursor: "move",
 	    helper: "clone",
-	    revert: "invalid"
+	    revert: "invalid",
+	    	start: function() {	        
+	        	console.debug('start draggable '+$(this).html());
+	      	},
+	      	drag: function() {
+				
+		    },
+	    	stop: function() {
+	        	console.debug('stop draggable'+$(this).html());
+	        	console.log(this);
+	      	}
 	});
+
+	$('#project').on('dblclick', '.component', function () {
+		console.debug('dblclick em componente já arrastado !!');
+		
+	})
 
 });
--- components.js
+++ components.js
@@ -1,3 +1,22 @@
-var components = {};
+var templates = {};
 
-components.button = '<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-unchecked"></span></button>';
+templates.inputForm = (function () {
+
+	var templ = '<div class="input-group component col-md-$largura$">'
+					+'<label>$label$</label>'
+					+'<input type="text" class="form-control" placeholder="$placeholder$">'
+				+'</div>';
+	return {
+		'templ' : templ
+	};
+})();
+
+templates.buttonForm = (function () {
+
+	var templ = '<div class="input-group component col-md-$largura$">'					
+					+'<button type="submit" class="btn btn-default">Submit</button>'
+				+'</div>';
+	return {
+		'templ' : templ
+	};
+})();
