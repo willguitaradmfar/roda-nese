eb859175843dff750a2fd58e5e543445e6a1deb2
William Lima <willguitaradmfar@gmail.com>
2014-09-18 16:29:10 -0300

    ::)))) grid

--- js/angular-app/app.js
+++ js/angular-app/app.js
@@ -2,6 +2,12 @@ angular.module('desenhador', [])
   .controller('desenhadorCtrl', ['$scope', function($scope) {
     console.debug('CHAMANDO CONTROLLER [desenhadorCtrl]');
 
-    $scope.model = 'Defatult name';
+    $scope.models = [];
+
+    $scope.models.push(
+    	{name : 'Lucas', age : 22, rg : '41.555'},
+    	{name : 'Ana', age : 26, rg : '654321'},
+    	{name : 'José', age : 32, rg : '123456'},
+    	{name : 'Marcos', age : 52, rg : '2135'});
     
   }]);
--- js/components/grid.js
+++ js/components/grid.js
@@ -2,13 +2,51 @@ var templates = templates || {};
 
 templates.grid = (function () {
 
-	var templ = '<table class="table"><thead><tr><th>testess<th><th>teste<th></tr></thead></table>';
+	var templ = '<table class="table table-hover">'
+					+'<thead><tr>'
+						+'<th>Nome<th>'
+						+'<th>Tel.<th>'
+					+'</tr></thead>'
+					+'<tbody>'
+						+'<tr data-ng-repeat="">'
+							+'<td>Maria<td>'
+							+'<td>11...<td>'
+						+'</tr>'
+					+'</tbody>'
+				+'</table>';
 
-	var property = {};		
+	var property = {};
+	property.cols = 'Nome,Idade,RG';
+	property.rows = 'name,age,rg';
+	property.collection = 'models';
+	property.filter = 'model';
 	
 
 	var update = function (target, comp) {
-		
+		if(comp.property.collection && comp.property.collection.length > 0){
+			$(target).find('tbody > tr').attr('data-ng-repeat', '_m in '+comp.property.collection+' | filter:'+comp.property.filter);
+		}
+
+		if(comp.property.cols && comp.property.cols.length > 0){			
+			var cols = [];
+			var _cols = comp.property.cols.split(',');
+			$(target).find('thead > tr').html('');
+			for(var i in _cols){
+				var col = _cols[i];
+				$(target).find('thead > tr').append('<th>'+col+'</th>');
+			}
+		}
+
+		if(comp.property.rows && comp.property.rows.length > 0){
+			var rows = [];
+			var _rows = comp.property.rows.split(',');
+			$(target).find('tbody > tr').html('');
+			for(var i in _rows){
+				var row = _rows[i];
+				$(target).find('tbody > tr').append('<td data-ng-bind="_m.'+row+'"></td>');
+			}
+		}
+
 	};
 
 	return {
