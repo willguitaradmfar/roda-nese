e547239587ef3ef34318c6e0c8cbb1a14236e96b
William Lima <willguitaradmfar@gmail.com>
2014-10-06 17:00:24 -0300

    ajuste

--- js/_db.js
+++ js/_db.js
@@ -1,146 +1,46 @@
 (function(global, undefined) {
-	var db = function () {
-		var _db = [];
-		var id = 0;
-		
-		var findOne = function (id) {
-			var hand;
-			if(typeof id == 'string'){
-				hand = function (obj, otherObj) {
-					return(obj.___id == otherObj);
-				}
-			}
-			if(!hand) throw 'HAND É INDEFINIDO';			
-			for(var i in _db){
-				var o = _db[i];
-				if(hand(o, id)){
-					return o;
-				}
-			}			
-		};
-
-		var update = function (query, newObject) {
-			var result = findOne(query);						
-			for(var i in newObject){
-				result[i] = newObject[i];
-			}
-		};
-
-		var bkp = function () {
-			return _db;
-		}
-
-		var restoreBkp = function (__db) {
-			_db = __db;
-		}
-
-		var remove = function (id) {
-			var index = -1;
-			for(var i in _db){
-				var obj = _db[i];
-				if(id == obj.___id){
-					index = i;
-				}
-			}
-
-			_db.splice(index, 1);
-			return 1;
-		};
-
-		var count = function (query) {
-			return find(query).length;
-		}
-		
-		var find = function (query) {
-
-			var hand = function (obj, otherObj) {
-				return (obj === otherObj);
-			};
-
-			if(typeof query == 'string'){
-				hand = function (obj, otherObj) {
-					return(obj.___id == otherObj);
-				}
-			}
-			var result = [];
-			for(var i in _db){
-				var o = _db[i];
-				if(hand(o, query)){
-					result.push(o);
-				}
-			}
-			return result;
-		};
-
-		var insert = function (doc) {
-			id++;
-			doc.___id = id;
-			_db.push(doc);
-			return doc;
-		};		
-
-		return {
-			insert : insert,
-			find : find,
-			findOne : findOne,
-			remove : remove,
-			count : count,
-			update : update,
-			bkp : bkp,
-			restoreBkp : restoreBkp
-		};
-	};
-
+	var db = TAFFY();	
 	global.desenhador = global.desenhador || {};
 	global.desenhador.db = global.desenhador.db || {};
-
-	var database = new db();
+		
 		
 	var insert = function (doc) {
-		 return database.insert(doc);
+		 return db.insert(doc).last();
 	};
-
-	var bkp = function () {
-		return database.bkp();
-	};
-
-	var restoreBkp = function (__db) {
-		database.restoreBkp(__db);
-	}
 
 	var find = function (query, hand) {
 		hand = hand || function (d) {
 			console.debug('ENCONTRADO : '+d.___id);
 		};
-		var t = database.find(query);
-		for(var i in t){
-			hand(t[i]);
-		}
+		var t = db(query);		
+		t.each(hand);		
 	};
 
 	var findOne = function(query) {
-		var t = database.findOne(query);
-		return t;
+		var t = db(query);		
+		return t.first();
 	};
 
 	var findOneById = function(id) {
-		var t = database.findOne(id);
-		return t;
+		if(!id) throw 'ID INDEFINIDO';
+
+		var t = db({___id : id});
+		return t.first();
 	};
 
 	var remove = function (id) {
 		if(!id) throw 'id indefinido'			
-		var r = database.remove(id);
+		var r = db(id).remove();
 		console.debug('DELETOU '+r+' REGISTRO(S)');
 		return r;
 	};
 
 	var count = function (query) {
-		return database.count(query);
+		return db(query).count();
 	};
 
 	var update = function (query, _update) {
-		return database.update(query, _update);
+		return db(query).update(_update);
 	};	
 
 	global.desenhador.db.insert = insert;
@@ -150,7 +50,11 @@
 	global.desenhador.db.remove = remove;
 	global.desenhador.db.findOne = findOne;
 	global.desenhador.db.findOneById = findOneById;
-	global.desenhador.db.bkp = bkp;
-	global.desenhador.db.restoreBkp = restoreBkp;
+
+	global.desenhador.db.show = function () {
+		db().each(function (i, o) {
+			  console.warn(i.___id, i);
+			})
+	};	
 	
 })(window);
--- js/db.js
+++ js/db.js
@@ -1,46 +1,146 @@
 (function(global, undefined) {
-	var db = TAFFY();	
+	var db = function () {
+		var _db = [];
+		var id = 0;
+		
+		var findOne = function (id) {
+			var hand;
+			if(typeof id == 'string'){
+				hand = function (obj, otherObj) {
+					return(obj.___id == otherObj);
+				}
+			}
+			if(!hand) throw 'HAND É INDEFINIDO';			
+			for(var i in _db){
+				var o = _db[i];
+				if(hand(o, id)){
+					return o;
+				}
+			}			
+		};
+
+		var update = function (query, newObject) {
+			var result = findOne(query);						
+			for(var i in newObject){
+				result[i] = newObject[i];
+			}
+		};
+
+		var bkp = function () {
+			return _db;
+		}
+
+		var restoreBkp = function (__db) {
+			_db = __db;
+		}
+
+		var remove = function (id) {
+			var index = -1;
+			for(var i in _db){
+				var obj = _db[i];
+				if(id == obj.___id){
+					index = i;
+				}
+			}
+
+			_db.splice(index, 1);
+			return 1;
+		};
+
+		var count = function (query) {
+			return find(query).length;
+		}
+		
+		var find = function (query) {
+
+			var hand = function (obj, otherObj) {
+				return (obj === otherObj);
+			};
+
+			if(typeof query == 'string'){
+				hand = function (obj, otherObj) {
+					return(obj.___id == otherObj);
+				}
+			}
+			var result = [];
+			for(var i in _db){
+				var o = _db[i];
+				if(hand(o, query)){
+					result.push(o);
+				}
+			}
+			return result;
+		};
+
+		var insert = function (doc) {
+			id++;
+			doc.___id = id;
+			_db.push(doc);
+			return doc;
+		};		
+
+		return {
+			insert : insert,
+			find : find,
+			findOne : findOne,
+			remove : remove,
+			count : count,
+			update : update,
+			bkp : bkp,
+			restoreBkp : restoreBkp
+		};
+	};
+
 	global.desenhador = global.desenhador || {};
 	global.desenhador.db = global.desenhador.db || {};
-		
+
+	var database = new db();
 		
 	var insert = function (doc) {
-		 return db.insert(doc).last();
+		 return database.insert(doc);
 	};
+
+	var bkp = function () {
+		return database.bkp();
+	};
+
+	var restoreBkp = function (__db) {
+		database.restoreBkp(__db);
+	}
 
 	var find = function (query, hand) {
 		hand = hand || function (d) {
 			console.debug('ENCONTRADO : '+d.___id);
 		};
-		var t = db(query);		
-		t.each(hand);		
+		var t = database.find(query);
+		for(var i in t){
+			hand(t[i]);
+		}
 	};
 
 	var findOne = function(query) {
-		var t = db(query);		
-		return t.first();
+		var t = database.findOne(query);
+		return t;
 	};
 
 	var findOneById = function(id) {
-		if(!id) throw 'ID INDEFINIDO';
-
-		var t = db({___id : id});
-		return t.first();
+		var t = database.findOne(id);
+		return t;
 	};
 
 	var remove = function (id) {
 		if(!id) throw 'id indefinido'			
-		var r = db(id).remove();
+		var r = database.remove(id);
 		console.debug('DELETOU '+r+' REGISTRO(S)');
 		return r;
 	};
 
 	var count = function (query) {
-		return db(query).count();
+		return database.count(query);
 	};
 
 	var update = function (query, _update) {
-		return db(query).update(_update);
+		return database.update(query, _update);
 	};	
 
 	global.desenhador.db.insert = insert;
@@ -50,11 +150,7 @@
 	global.desenhador.db.remove = remove;
 	global.desenhador.db.findOne = findOne;
 	global.desenhador.db.findOneById = findOneById;
-
-	global.desenhador.db.show = function () {
-		db().each(function (i, o) {
-			  console.warn(i.___id, i);
-			})
-	};	
+	global.desenhador.db.bkp = bkp;
+	global.desenhador.db.restoreBkp = restoreBkp;
 	
 })(window);
